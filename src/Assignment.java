e74fca5e78373f03d55ce3fc7a45e5860ffa0b7e
ajith <ajithdrlg@gmail.com>
2024-09-16 14:43:54 +0530

    first commit

---
+++ src/Assignment.java
@@ -0,0 +1,69 @@
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium driver\\chromedriver-win64\\chromedriver-win64");
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+
+		String[] names = { "iphone", "Samsung", "Nokia", "Blackberry" };
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(3));
+
+		driver.findElement(By.cssSelector("input[name='username']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("#password")).sendKeys("learning");
+		driver.findElement(By.xpath("//label/input[@value='user']")).click();
+
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[id='okayBtn']")));
+		driver.findElement(By.cssSelector("button[id='okayBtn']")).click();
+
+		WebElement dropdown = driver.findElement(By.xpath("//select[@class='form-control']"));
+		Select drop = new Select(dropdown);
+		drop.selectByVisibleText("Consultant");
+		driver.findElement(By.id("terms")).click();
+		driver.findElement(By.cssSelector("input[class*='btn']")).click();
+
+		w.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//a[contains(text(), 'ProtoCommerce Home')] ")));
+		driver.findElement(By.xpath("//a[contains(text(), 'ProtoCommerce Home')] "));
+		additems(driver, names);
+		driver.findElement(By.xpath("//a[@class='nav-link btn btn-primary']")).click();
+
+	}
+
+	public static void additems(WebDriver driver, String[] names) {
+		int j = 0;
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.card-title"));
+
+		for (int i = 0; i < products.size(); i++) {
+			String[] split = products.get(i).getText().split(" ");
+			String formattedname = split[0].trim();
+
+			List device = Arrays.asList(names);
+
+			if (device.contains(formattedname)) {
+				j++;
+				driver.findElements(By.cssSelector("button[class*='btn']")).get(i).click();
+				if (j == names.length) {
+					break;
+				}
+
+			}
+
+		}
+
+	}
+
+}
---
+++ src/typesofwait.java
@@ -0,0 +1,82 @@
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class typesofwait {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium driver\\chromedriver-win64\\chromedriver-win64.exe");
+		driver.manage().window().maximize();
+		//explicit wait
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
+		
+		//implicit wait
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		String[] names = {"Cucumber", "Brocolli", "Beetroot", "Carrot"};
+		
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		
+		//Thread.sleep(3000);
+		System.out.println(driver.getTitle());
+		additems(driver, names);
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.xpath("//button[contains(text(), 'PROCEED TO CHECKOUT')]")).click();
+		
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[class='promoCode']")));
+		driver.findElement(By.cssSelector("input[class='promoCode']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector(".promoBtn")).click();
+		//explicit wait
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class='promoInfo']")));
+		
+				
+		System.out.println(driver.findElement(By.cssSelector("span[class='promoInfo']")).getText());
+
+	}
+	public static void additems(WebDriver driver, String[] names)
+	{
+		int j =0;
+		List <WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+		
+		
+		for(int i=0;i<products.size();i++)
+		{
+			String[] name = products.get(i).getText().split("-");
+			String formattedname = name[0].trim();
+			//format the name to get the actual vegetable name
+			//Convert array into array list using easy search
+			//Check whether the name you extracted is present in the array list or not
+			
+			List items = Arrays.asList(names);
+			
+			if(items.contains(formattedname))
+					{
+				//add to cart
+				j++;
+				driver.findElements(By.xpath("//div[@class='product-action']")).get(i).click();
+				if(j==names.length)
+				{
+					break;
+				}
+				
+				
+				
+					}
+			
+			
+		}
+		
+		
+	}
+
+}
